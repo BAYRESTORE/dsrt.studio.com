48592674b2d65a867dcd0a42a3020194ecab2c94
BAYRESTORE <prbayuu@gmail.com>
2025-08-02 19:41:30 +0700

    Update restore.js

--- api/restore.js
+++ api/restore.js
@@ -1,5 +1,4 @@
-// File: api/restore.js
-
+// api/restore.js
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
